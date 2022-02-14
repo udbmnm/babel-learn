6a0d22631099b1816b1481b3932a8308a075981f
Kevin <kaizheng@happybears.net>
2022-02-14 11:58:07 +0800

    infer

--- type.ts
+++ type.ts
@@ -29,10 +29,24 @@ type PickOne<T, K extends keyof T> = { [key in K]: T[key] };
 type ExcludeOne<T, K extends keyof T> = { [key in Exclude<keyof T, K>]: T[key] };
 // type UppercaseKeys<T ,T0 = {[K in keyof T]: ToUppercase<K>},T1 = SwitchKeyValue<T0>,T2 = {[K in keyof T1]: T[ToLowerCase<K>]}> = T2;
 
+//https://jkchao.github.io/typescript-book-chinese/tips/infer.html#%E4%BB%8B%E7%BB%8D
+// infer 表示在 extends 条件语句中待推断的类型变量
+//在这个条件语句 T extends (...args: infer P) => any ? P : T 中，infer P 表示待推断的函数参数。
+// 整句表示为：如果 T 能赋值给 (...args: infer P) => any，则结果是 (...args: infer P) => any 类型中的参数 P，否则返回为 T。
+
+type ParamType<T> = T extends (...args: infer P) => any ? P : T;
+type GetType<T> = T extends infer P ? P : never;
+
+type anyType = GetType<number>;
+
 //获取索引签名 key
 type InferTypeKey<T,K,K1 = ToLowerCase<K>> = K1 extends keyof T ? T[K1] : never
-
 type UppercaseType<T> = {[K in ToUppercase<keyof T>]: InferTypeKey<T,K> };
+
+type Func = (user: Student) => void;
+
+type Param = ParamType<Func>;
+type AA = ParamType<string>; // string
 
 type S6 = PickOne<Student, 'age'>;
 type S7 = ExcludeOne<Student, 'age'>;
@@ -41,6 +55,8 @@ type S9 = keyof Student[];
 type S10 = {[key in Uppercase<keyof Student>]: Student[Lowercase<key>] };
 type S11 = UppercaseType<Student>;
 type S12 = GetKeyType<Student, 'age'>;
+
+
 
 
 const a: S8 = 'age';
